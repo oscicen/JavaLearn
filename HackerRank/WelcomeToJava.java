001da1f07ee154ea42b93cb4c5ba76deedcf73fd
oscicen <oscicen@gmail.com>
2018-12-31 13:16:21 +0200

    Firts files

---
+++ HackerRank/WelcomeToJava.java
@@ -0,0 +1,8 @@
+public class WelcomeToJava { //For the code to work on hackerrank this class with "Solution"
+
+	public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+	}
+
+}
