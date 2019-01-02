c637e38e27bb83402eaf6d5f10a914de06675e7f
oscicen <oscicen@gmail.com>
2019-01-02 13:46:42 +0200

    Daily 1/2/2019

--- HackerRank/WelcomeToJava.java
+++ HackerRank/WelcomeToJava.java
@@ -1,8 +1,8 @@
-public class WelcomeToJava { //For the code to work on hackerrank this class with "Solution"
+public class WelcomeToJava {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello, World.");
         System.out.println("Hello, Java.");
-	}
-
+    }
+    
 }
