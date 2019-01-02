a96c242e72af100d07ea3698c34afcaa4ea37429
oscicen <oscicen@gmail.com>
2019-01-02 14:53:50 +0200

    Fixed game

--- HeadFirstJava/BeerSong.java
+++ HeadFirstJava/BeerSong.java
@@ -7,15 +7,15 @@ public class BeerSong {
 		
 		while(beerNum > 0) {
 			
-			if(beerNum == 1) {
-				word = "bottle"; // singular
-			}
-			
 			System.out.println(beerNum + " " + word + " of beer on the wall.");
 			System.out.println(beerNum + " " + word + " of beer.");
 			System.out.println("Take one down.");
 			System.out.println("Pass it around.");
 			beerNum = beerNum - 1;
+			
+			if(beerNum == 1) {
+				word = "bottle"; // singular
+			}
 			
 			if(beerNum > 0) {
 				System.out.println(beerNum + " " + word + " of beer on the wall.");
