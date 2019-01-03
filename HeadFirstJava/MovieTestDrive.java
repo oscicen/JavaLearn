74f70a328b8fed09118b909c376153d12260e110
oscicen <oscicen@gmail.com>
2019-01-03 09:30:13 +0200

    More on classes

---
+++ HeadFirstJava/MovieTestDrive.java
@@ -0,0 +1,22 @@
+public class MovieTestDrive {
+	
+	public static void main(String[] args) {
+		
+		Movie one = new Movie();
+		one.title = "Gone with the stock";
+		one.genre = "Tragic";
+		one.rating = -2;
+		
+		Movie two = new Movie();
+		two.title = "Lost in cubicle space";
+		two.genre = "Comedy";
+		two.rating = 5;
+		
+		Movie three = new Movie();
+		three.title = "Byte club";
+		three.genre = "Tragic but ultimatly uplifting";
+		three.rating = 127;
+		
+	}
+	
+}
